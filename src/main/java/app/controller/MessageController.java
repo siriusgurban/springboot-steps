697c7a7459f12ce70a164a3aee263758fbef57ce
Isgkha <xalidisgenderov1@gmail.com>
2019-12-10 14:29:27 +0400

    asda

---
+++ src/main/java/app/controller/MessageController.java
@@ -0,0 +1,20 @@
+package app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMspping;
+
+@Controller
+@RequestMapping("/msg")
+public class MessageController {
+
+    @GetMapping
+    public String handle_get(){
+        return "hello";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String handle_post(){
+        return "hello_msg_template";
+    }
+
+}
